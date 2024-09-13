bab8fca18723ee77b93c5cb2be703e3b97bf141c
Isurulak1 <isurulakshan0417@gmail.com>
2024-09-13 20:41:07 +0530

    install and configure tailwind CSS

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+//Middleware
+app.use("/",(req,res,next)=>{
+    res.send("Its working");
+})
+
+mongoose.connect("mongodb+srv://Crud_admin:0GKX3JiJMOKGO3nd@cluster0.dqkdy.mongodb.net/")
+.then(()=>console.log("Connected to database"))
+.then(()=>{
+    app.listen(5000);
+})
+.catch((err)=>console.log((err)));
