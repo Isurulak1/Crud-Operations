297056c6bda3c3aaf47e51ea3aefb7cd4b1ca3c8
Isurulak1 <isurulakshan0417@gmail.com>
2024-09-16 12:29:48 +0530

    Update and delete

--- Controllers/UserControl.js
+++ Controllers/UserControl.js
@@ -67,6 +67,45 @@ const getById = async (req,res, next) => {
 
 }
 
+//Update user details
+const UpdateUser = async (req, res, next) => {
+
+    const id = req.params.id;
+    const{ name, gmail, age, address} = req.body;
+
+    let users;
+
+    try{
+        users = await User.findByIdAndUpdate(id,{ name: name, gmail: gmail, age: age, address: address });
+        users = await users.save();
+    }catch{
+        console.log(err);
+    }
+
+    if(!users){
+        return res.status(404).json({ message: "Unable to update details" });
+    }
+    return res.status(200).json({ users });
+}
+
+const deleteUser = async (req, res, next) => {
+    const id = req.params.id;
+    let user;
+
+    try{
+        user = await User.findByIdAndDelete(id);
+    }catch(err){
+        console.log(err);
+    }
+
+    if(!user){
+        return res.status(404).json({ message: "Unable to delete details" });
+    }
+    return res.status(200).json({ user });
+}
+
 exports.getAllUsers = getAllUsers;
 exports.addUsers = addUsers;
 exports.getById = getById;
+exports.UpdateUser = UpdateUser;
+exports.deleteUser = deleteUser;
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -8,6 +8,8 @@ const UserController = require('../Controllers/UserControl');
 router.get('/',UserController.getAllUsers);
 router.post('/',UserController.addUsers);
 router.get('/:id',UserController.getById);
+router.put('/:id',UserController.UpdateUser);
+router.delete('/:id',UserController.deleteUser);
 
 
 module.exports = router;
