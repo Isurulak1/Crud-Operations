d45a1bf23619de30e2af0bd61652c7ffd82a06f6
Isurulak1 <isurulakshan0417@gmail.com>
2024-09-16 11:10:09 +0530

    Retrieve Data by ID Using Postman

--- Controllers/UserControl.js
+++ Controllers/UserControl.js
@@ -48,5 +48,25 @@ const addUsers = async (req, res, next) => {
 
 };
 
+//Get by Id
+
+const getById = async (req,res, next) => {
+    const id = req.params.id;
+    let user;
+
+    try{
+        user = await User.findById(id);
+    }catch(err){
+        console.log(err);
+    }
+
+    if(!user){
+        return res.status(404).json({ message: "user not found" });
+    }
+    return res.status(200).json({ user });
+
+}
+
 exports.getAllUsers = getAllUsers;
 exports.addUsers = addUsers;
+exports.getById = getById;
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -7,6 +7,7 @@ const UserController = require('../Controllers/UserControl');
 
 router.get('/',UserController.getAllUsers);
 router.post('/',UserController.addUsers);
+router.get('/:id',UserController.getById);
 
 
 module.exports = router;
